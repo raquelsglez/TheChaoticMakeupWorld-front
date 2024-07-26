e1353a199c6e170f9e6f7659f1257ab11ae486f1
Raquel <raquelsglez99@gmail.com>
2024-07-26 18:10:09 +0200

    feat: add button back home

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -4,13 +4,18 @@ import styles from "./Root.module.css"
 import { FaUser } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
 import { usePostContext } from "../context/PostContext";
+import { RiHomeHeartFill } from "react-icons/ri";
 
 
 function Root() {
   const { user } = usePostContext(); //contexto para obtener user
   const navigate = useNavigate();
 
-  const handleClick = () => {
+  const handleClickHome = () => {
+    navigate('/');
+  };
+
+  const handleClickProfile = () => {
     if(user.username){ // al hacer click en el icono de usuario -> si el user esta logdueado navegar al profile
       navigate('/profile');
       return;
@@ -24,13 +29,19 @@ function Root() {
       <nav className={styles.navBar}>
         <ul>
           <li>
+            <RiHomeHeartFill onClick={handleClickHome} className={styles.homeIcon} size={35}/>
+          </li>
+          <li>
             <Link to="/">THE CHAOTIC MAKEUP WORLD</Link>
           </li>
+          <li>
+            <div onClick={handleClickProfile} className={styles.profileIcon}>
+              <FaUser/>
+              <p>{user.username}</p>
+            </div> 
+          </li>
         </ul>
-        <div onClick={handleClick} className={styles.profileIcon}>
-          <FaUser/>
-          <p>{user.username}</p>
-        </div> 
+
       </nav>
       <Outlet/>
     </>
